123abf63c25aafa6d635809360b401f6fd010472
RooT_Kim <bdd3452@gmail.com>
2022-07-11 23:50:09 +0900

    Editted index.ts

--- practice/index.ts
+++ practice/index.ts
@@ -1,3 +1,11 @@
-console.log('hello world');
+import fs from 'fs/promises'
 
 const num : number = 1;
+
+function sayHelloTo(target: string) {
+  console.log(`Hello, ${target}`);
+}
+
+sayHelloTo("Me");
+
+console.log('hello world');
