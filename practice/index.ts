480a80b620aa3dd86b5829983c103622453fa59a
RooT_Kim <bdd3452@gmail.com>
2022-07-13 22:27:38 +0900

    Updated index.ts

--- practice/index.ts
+++ practice/index.ts
@@ -1,19 +1,24 @@
+// const fs = require("fs/promises");
 import fs from 'fs/promises';
 
+console.log('hello world');
 const num : number = 1;
+let sum = 0;
 
 function sayHelloTo(target: string) {
   console.log(`Hello, ${target}`);
 }
 
-sayHelloTo("Me");
-
-console.log('hello world');
-
-function addFunction(num1: number, num2: number): number {
-  return num1 + num2;
+function addTwoValues(first: number, second: number, third: number): number {
+  return first + second + third;
 }
 
-let sum = addFunction(5,5);
+async function main() {
+  sayHelloTo('World');
+  sum = addTwoValues(num, 100, 1000);
+  const samples = await fs.readFile('./sample.json','utf8');
+  console.log(samples);
+  console.log(JSON.parse(samples));
+}
 
-console.log(sum);
+main();
